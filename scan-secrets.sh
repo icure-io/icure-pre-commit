d55bf2bfd04b6edfca10db21178b23ef892886ed
Luca Tremamunno <luca@icure.com>
2022-08-08 17:47:38 +0200

    Add scan-secrets script with all filters and plugins

---
+++ scan-secrets.sh
@@ -0,0 +1,6 @@
+detect-secrets scan \
+  --filter ./icure-pre-commit/icure_detect_secrets/filters/is_icure_safe_data.py::is_icure_safe_data \
+  --filter ./icure-pre-commit/icure_detect_secrets/filters/is_key_format.py::is_key_format \
+  --plugin icure-pre-commit/icure_detect_secrets/plugins/EncodedFileDetector.py \
+  --plugin icure-pre-commit/icure_detect_secrets/plugins/KeyFileDetector.py \
+  "$@" > .secrets.baseline
