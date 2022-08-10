f06bf57f39cdce1a8929a0f5e1113a9ff3bee492
Luca Tremamunno <luca@icure.com>
2022-08-10 17:26:39 +0200

    Update required install script

--- install_required_tools.sh
+++ install_required_tools.sh
@@ -11,12 +11,12 @@ if has_pre_commit ; then
 fi
 
 do_pip() {
-  if command -v pip &> /dev/null ; then
-    command pip "$@"
-  elif command -v pip3 $> /dev/null ; then
+  if command -v pip3 $> /dev/null ; then
     command pip3 "$@"
+  elif command -v pip &> /dev/null ; then
+    command pip "$@"
   else
-    echo "ERROR: 'pip' is required to install pre-commit hooks. Make sure that either 'pip' or 'pip3' is installed and in PATH."
+    echo "ERROR: 'pip' is required to install detect-secrets. Make sure that either 'pip3' or 'pip' is installed and in PATH."
     echo "https://pip.pypa.io/en/stable/installation/"
     exit 1
   fi
@@ -25,11 +25,11 @@ do_pip() {
 echo "Installing required tools"
 
 do_pip install pre-commit==2.20.0
+retval=$?
 
-if ! [[ $? ]] ; then
-  exit_val=$?
+if [ $retval -eq 0 ]; then
+  echo "Installation successful! Run 'pre-commit install' on the root of the repository to install pre-commit hooks."
+else
   echo "ERROR: Installation failed"
-  exit $exit_val
+  exit $retval
 fi
-
-echo "Installation successful! Run 'pre-commit install' on the root of the repository to install pre-commit hooks."
